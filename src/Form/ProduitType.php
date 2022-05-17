aecab6c79bf9ffcb2325ed323c4138502e04a9db
Arona NGOM <arona.ngom07@gmail.com>
2022-05-17 15:34:01 +0200

    Project basic features

---
+++ src/Form/ProduitType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Doctrine\DBAL\Types\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
+
+class ProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prix')
+            ->add('dateLancement')
+            ->add('photo')
+            ->add('stock')
+            ->add('disponible')
+            ->add('typeProduit')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
